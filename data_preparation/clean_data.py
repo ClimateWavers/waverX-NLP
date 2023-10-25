79ee6f559a7c2609e73fd5a9d543f033205f3723
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-25 23:28:51 +0100

    Waverx-nlp

--- data_preparation/clean_data.py
+++ data_preparation/clean_data.py
@@ -8,20 +8,20 @@ def filter_data(input_file, output_file):
     # Read CSV file into a DataFrame
     df = pd.read_csv(input_file)
 
-    #Make sure all data are of same type of string
-    df['text'] = df['text'].astype(str)
-    df['label'] = df['label'].astype(str)
     # Drop rows where target column is 0
     cleaned_df = df[df['target'] != 0]
-
-
-
     # Write cleaned DataFrame back to a CSV file
     cleaned_df.to_csv(output_file, index=False)
 
     print(f"Cleaned data saved to {output_file}")
 
 
+def add_data_type(input_file):
+    df = pd.read_csv(input_file)
+    #Make sure all data are of same type of string
+    df['text'] = df['text'].astype(str)
+    df['label'] = df['label'].astype(str)
+    print("Ensured all data are of type str")
 # Example usage
 if __name__ == "__main__":
     # Provide input and output file paths
@@ -29,3 +29,4 @@ if __name__ == "__main__":
     disaster_dataset = f"{base_dir}/tweets.csv"
     # Filter Data
     filter_data(disaster_dataset, disaster_dataset)
+    add_data_type("dataset/disaster_text.csv")
